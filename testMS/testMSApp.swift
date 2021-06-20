9c750299b6129038fc140c8e5bc59c70dbcf070b
1gr0k <43414453+1gr0k@users.noreply.github.com>
2021-06-21 00:08:56 +0400

    Initial Commit

---
+++ testMS/testMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  testMSApp.swift
+//  testMS
+//
+//  Created by Андрей Калямин on 21.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct testMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
